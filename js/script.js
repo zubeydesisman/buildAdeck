2a11995eaa8f2d91e8af1523054789da8e1357b9
mk and zs <mk@>
2019-08-20 10:35:09 -0700

    Add for loop instead of forEach method

--- js/script.js
+++ js/script.js
@@ -4,11 +4,16 @@ $(document).ready(function() {
    var ranks = ["ace",2,3,4,5,6,7,8,9,10,"jack","queen","king"];
    var suits = ["clubs","diamonds","spades","hearts"];
    var cards = [];
-     ranks.forEach(function(rank){
-      suits.forEach(function(suit){
-       cards.push(rank + " of " + suit);
-     });
-   });
+   //   ranks.forEach(function(rank){
+   //    suits.forEach(function(suit){
+   //     cards.push(rank + " of " + suit);
+   //   });
+   // });
+    for(var i = 0; i < ranks.length; i++) {
+      for(var j = 0; j < suits.length; j++) {
+        cards.push(ranks[i] + " of " + suits[j]);
+      }
+    }
      $("#orderedlist").text(cards);
   });
 });
